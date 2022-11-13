69e9c77292fce970dc1c402707eddf4ed139422c
Vladislav Marinin <czargion@gmail.com>
2022-11-13 23:41:20 +0200

    add solution

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+let items = "";
+galleryItems.forEach(({ preview, original, description } = {}) => {
+    items += `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+});
+
+gallery.innerHTML = items;
+
+function createModalImg(url, alt) {
+    const modalImg = basicLightbox.create(`<img src="${url}" alt="${alt}" />`, {
+        onShow: () => {
+            document.addEventListener("keydown", closeOnEscBtn);
+        },
+        onClose: () => {
+            document.removeEventListener("keydown", closeOnEscBtn);
+        },
+    });
+    function closeOnEscBtn(event) {
+        if (event.key === "Escape") {
+            modalImg.close();
+        }
+    }
+    return modalImg;
+}
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    const { target } = event;
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+    const imgUrl = target.dataset.source;
+    const imgAlt = target.alt;
+    const modalImg = createModalImg(imgUrl, imgAlt);
+    modalImg.show();
+}
+
+gallery.addEventListener("click", onGalleryItemClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+let items = "";
+galleryItems.forEach(({ preview, original, description } = {}) => {
+    items += `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+});
+
+gallery.innerHTML = items;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+});
+console.dir(lightbox);
